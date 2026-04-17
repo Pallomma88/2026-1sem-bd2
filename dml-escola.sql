54df3198f4639c3912f82a7e58001ce78f36a794
Pallomma88 <pallomma88@gmail.com>
2026-04-17 20:30:52 -0300

    17/04/2026

--- dml-escola.sql
+++ dml-escola.sql
@@ -14,3 +14,41 @@ Email VARCHAR(200) UNIQUE
 
 INSERT INTO Alunos ( Nome, Idade, Email)
 	VALUES ('Joao', 26, 'joao@email.com');
+
+INSERT INTO Alunos ( Nome, Idade, Email)
+	VALUES
+		('Luiza', 13, 'luiza@email.com'),
+		('Manoel', 32, 'manoel@email.com')
+;
+
+-- tomar cuidado ao realizar UPDATE
+--UPDATE Alunos 
+--SET Idade = 19;
+
+
+UPDATE Alunos 
+	SET Idade = 13
+	WHERE Nome = 'Luiza'
+;
+
+UPDATE Alunos 
+	SET Idade = 26
+	WHERE Nome = 'Manoel'
+;
+
+
+INSERT INTO Alunos ( Nome, Idade, Email)
+	VALUES
+		('Manoel', 17, 'manoel2@email.com')
+;
+
+INSERT INTO Alunos ( Nome, Idade, Email)
+	VALUES
+		('Pallomma',37, 'palomma8@email.com')
+;
+
+
+UPDATE Alunos 
+	SET Idade = 32
+	WHERE Nome = 'Manoel'
+;
